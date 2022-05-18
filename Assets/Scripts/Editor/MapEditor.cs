512e23914356daf00f5687c41b8a00ea3cf86a16
longnh176040 <thefollowingday.2099@gmail.com>
2022-05-18 22:37:13 +0700

    add array plugins

---
+++ Assets/Scripts/Editor/MapEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MapEditor : EditorWindow
+{
+    [MenuItem("MapGenerator/Map Generator")]
+    public static void ShowWindow()
+    {
+        GetWindow<MapEditor>("Map Generator");
+    }
+
+    void Update()
+    {
+        
+    }
+}
